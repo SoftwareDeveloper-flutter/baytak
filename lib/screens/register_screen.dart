dab26c7c07c5458658e3b4653c0ac4514df6a942
SoftwareDeveloper-flutter <anilamuhammadashraf.software@gmail.com>
2024-12-07 22:48:00 +0500

    signup page design completed

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -1,78 +1,150 @@
 import 'package:flutter/material.dart';
-class RegisterScreen extends StatefulWidget{
+
+class RegisterScreen extends StatefulWidget {
   const RegisterScreen({super.key});
   @override
- State<RegisterScreen> createState()=>_RegisterScreenState();
+  State<RegisterScreen> createState() => _RegisterScreenState();
 }
 
-class _RegisterScreenState extends State<RegisterScreen>{
-  @override 
-  Widget build(BuildContext context){
+class _RegisterScreenState extends State<RegisterScreen> {
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-           body:Container(
-            height:MediaQuery.of(context).size.height,
-            width:MediaQuery.of(context).size.width,
-            decoration: const BoxDecoration(
-             color:Colors.white
-            ),
-        
-            
-            child:  Padding(
-              padding: const EdgeInsets.only(top:30.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                 crossAxisAlignment: CrossAxisAlignment.center,
-                 children: [
-                  ClipOval(
-                   child:Image.asset('assets/images/logo.png',height:200,width:200),
-                  ),
-                 const Text("Sign Up Here!",style:TextStyle(fontSize:20,fontWeight:FontWeight.w900)),
-                 const Padding(
-                   padding:  EdgeInsets.symmetric(horizontal: 30),
-                   child: Column(
-                    children: [
-                          ReuseableSocialContainer(color:Color(0xff2684FB),asset:'assets/images/fb_icon.png',title:"Sign up with facebook"),
-                          ReuseableSocialContainer(color:Color.fromARGB(255, 245, 63, 59),asset:'assets/images/google_icon.png',title:"Sign up with google"),
-
-                    ],
-                   ),
-                   
-                   
-                 ),
-                 ],
-             
+      body: SingleChildScrollView(
+      child:Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        decoration: const BoxDecoration(color: Colors.white),
+        child: Padding(
+          padding: const EdgeInsets.only(top: 30.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              ClipOval(
+                child: Image.asset('assets/images/logo.png',
+                    height: 150, width: 150),
               ),
+              const Text("Sign Up Here!",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w900)),
+               Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 30),
+                child: Column(
+                  children: [
+                    const ReuseableSocialContainer(
+                        color: Color(0xff2684FB),
+                        asset: 'assets/images/fb_icon.png',
+                        title: "Sign up with facebook"),
+                  const  ReuseableSocialContainer(
+                        color: Color.fromARGB(255, 245, 63, 59),
+                        asset: 'assets/images/google_icon.png',
+                        title: "Sign up with google"),
+                   const Row(
+                      children: [
+                        Expanded(child: Divider()),
+                        SizedBox(width: 30),
+                        Text("OR", style: TextStyle(fontSize: 20)),
+                        SizedBox(width: 30),
+                        Expanded(child: Divider()),
+                      ],
+                    ),
+                    Column(
+                     crossAxisAlignment: CrossAxisAlignment.start,
+                     mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                       const Text("Username",style:TextStyle(color:Color(0xff8F92A1))),
+                         const SizedBox(height:5),
+                        Container(
+                          decoration:BoxDecoration(
+                            color:const Color(0xff8592a1).withOpacity(0.1),
+                            borderRadius: BorderRadius.circular(10)
+                          ),
+                        child: TextFormField(
+                          decoration: const InputDecoration(
+                               enabledBorder: InputBorder.none,
+                               focusedBorder: InputBorder.none,
+                               disabledBorder: InputBorder.none,
+                              errorBorder: InputBorder.none,
+                          ),
+                        )
+                      
+                        ),
 
-            ),
-           ),
+                         const Text("Password",style:TextStyle(color:Color(0xff8F92A1))),
+                         const SizedBox(height:5),
+                        Container(
+                          decoration:BoxDecoration(
+                            color:const Color(0xff8592a1).withOpacity(0.1),
+                            borderRadius: BorderRadius.circular(10)
+                          ),
+                        child: TextFormField(
+                          decoration: const InputDecoration(
+                            suffix: Icon(Icons.visibility_off,color:Colors.red),
+                               enabledBorder: InputBorder.none,
+                               focusedBorder: InputBorder.none,
+                               disabledBorder: InputBorder.none,
+                              errorBorder: InputBorder.none,
+                          ),
+                        )
+                      
+                        ),
+                      ],
+                    ),
+
+                   const SizedBox(height:10),
+                     Material(
+                      color: Colors.black,
+                      elevation: 6,
+                      borderRadius: BorderRadius.circular(8),
+                      child:MaterialButton(
+                        minWidth: double.infinity,
+                        onPressed:(){},
+      
+                        child:const Text("Sign Up",style:TextStyle(color:Colors.white))
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      ),
     );
   }
 }
 
-class ReuseableSocialContainer extends StatelessWidget{
-  final String title,asset;
+class ReuseableSocialContainer extends StatelessWidget {
+  final String title, asset;
   final Color color;
-   const ReuseableSocialContainer({super.key ,required this.color,required this.title,required this.asset});
+  const ReuseableSocialContainer(
+      {super.key,
+      required this.color,
+      required this.title,
+      required this.asset});
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 5),
       child: Container(
-        height:50,
-        width:double.infinity,
+        height: 50,
+        width: double.infinity,
         decoration: BoxDecoration(
-          color:color,
+          color: color,
           borderRadius: BorderRadius.circular(5),
         ),
-        child:Padding(
-          padding:const EdgeInsets.only(left:20),
-        child:Row(
-          children: [
-            Image(image:AssetImage(asset),),
-            const SizedBox(width:20),
-            Text(title,style:const TextStyle(color:Colors.white))
-          ],
-        ),
+        child: Padding(
+          padding: const EdgeInsets.only(left: 20),
+          child: Row(
+            children: [
+              Image(
+                image: AssetImage(asset),
+              ),
+              const SizedBox(width: 20),
+              Text(title, style: const TextStyle(color: Colors.white))
+            ],
+          ),
         ),
       ),
     );
